abd175d689da0dc03124833481919179fc9c233a
贾思贤 <jiasixian@C02WD2FVG8WN.local>
2018-06-01 10:29:23 +0800

    行情demo开发，熟悉lisener

---
+++ src/main/java/hello/MarketService.java
@@ -0,0 +1,13 @@
+package hello;
+
+import hello.Monitor.MarketLisener;
+
+/**
+ * 行情API开发
+ */
+public class MarketService {
+
+private MarketLisener lisener;
+
+
+}
---
+++ src/main/java/hello/Monitor/MarketLisener.java
@@ -0,0 +1,8 @@
+package hello.Monitor;
+
+/**
+ * 行情监听器
+ */
+public class MarketLisener {
+
+}
